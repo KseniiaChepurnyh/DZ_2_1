3b0419bde00e0ce06a009ac07af0955f0c8c188f
Ксения Чепурных <kseniyache97@gmail.ru>
2020-11-16 22:18:18 +0500

    Task 3 was complete

---
+++ DZ_2_1/DZ_2_1/BlueViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BlueViewController.swift
+//  DZ_2_1
+//
+//  Created by Ксения Чепурных on 16.11.2020.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func dismiss(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ DZ_2_1/DZ_2_1/RedViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RedViewController.swift
+//  DZ_2_1
+//
+//  Created by Ксения Чепурных on 16.11.2020.
+//
+
+import UIKit
+
+class RedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func dismiss(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
